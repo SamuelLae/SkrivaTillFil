d691886a6832ac4be51b2c1c5aaf40de1c7f55a4
samuel <samuel.laestander@edu.lycksele.se>
2025-01-10 13:41:47 +0100

    first commit

---
+++ SkrivaTillFilTest/Program.cs
@@ -0,0 +1,9 @@
+﻿FileStream fs = new FileStream("FilePath.txt", FileMode.Open, FileAccess.Read);
+
+StreamReader reader = new (fs);
+
+string fileContent = reader.ReadToEnd();
+
+reader.Close();
+
+Console.WriteLine(fileContent);
